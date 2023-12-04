d0a59571ded092b03bb914311e72b37399330967
Alex Leto <alle5715@login11.rc.colorado.edu>
2023-12-04 11:44:57 -0700

    updated masked_runner for blanca

--- scripts/masked_runner.sh
+++ scripts/masked_runner.sh
@@ -2,13 +2,17 @@
 
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
+#SBATCH --mem=16G
 #SBATCH --time=24:00:00
-#SBATCH --partition=aa100
+#SBATCH --account=blanca-curc-gpu
+#SBATCH --qos=blanca-curc-gpu
+#SBATCH --partition=blanca-curc-gpu
 #SBATCH --gres=gpu:1
 #SBATCH --output=masked_tune-%j.out
 #SBATCH --mail-type="ALL"
 #SBATCH --mail-user="alle5715@colorado.edu"
 
+mkdir -p logs
 nvidia-smi >> logs/nvidia-smi.out
 
 source /home/${USER}/.bashrc
