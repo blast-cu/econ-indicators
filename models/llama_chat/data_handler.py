542fc2b55d4fa100dc64dcae15346678834b2d73
Dananjay Srinivas <dananjay.srinivas@gmail.com>
2023-12-10 16:50:32 +0530

    llama-chat first REAL commit 🤞

---
+++ models/llama_chat/data_handler.py
@@ -0,0 +1,127 @@
+import pickle
+import random
+
+class LlamaChatPromptGenerator(object) :
+
+    prompt = '''
+Take a look at the following definitions of economic domains that newspaper excerpts cover.
+
+[DEFINITIONS]
+
+Based on the definitions, you can assign a label to a newspaper excerpt. For example:
+
+[EXAMPLES]
+
+Using the definitions of these economic indicators, what is the label for the following newspaper excerpt:
+
+[EXCERPT_TO_PREDICT]
+    '''
+
+    label_definitions = '''
+macro: macro-economic indicators aggregate statistics and data that reflect the economic\
+circumstances of a particular country or region.
+
+business: A business-specific data point is a data point associated with a particular firm or company.
+
+industry: Industry level data points describe an entire industry rather than individual businesses
+
+government: Any value that describes how a government earned or spent its income falls into this category.
+
+personal: If data point focuses on the economic condition of a single person, or a group of individuals that \
+    is not large enough to represent an entire demographic then we consider it personal
+    '''
+
+    def __init__(self, path_to_splits_dict:str="data/splits_dict", 
+                 path_to_quant_dict:str="data/quant_dict",
+                 path_to_save_predictions:str="data/preds_dict",
+                 train_split_to_use_for_prompt_example:int=0) -> None:
+        
+        self.splits = pickle.load(open(path_to_splits_dict , "rb"))
+        self.excerpts = pickle.load(open(path_to_quant_dict , "rb"))
+        self.path_to_save_predictions = path_to_save_predictions
+        self.predicted_excerpts = self.excerpts
+        self.train_split_to_use_for_prompt_example = train_split_to_use_for_prompt_example
+        self.examples_for_each_label = self.get_examples_for_each_label()
+
+    def generator(self, test_split:int) : 
+
+        excerpt_ids = self.splits[test_split]["test"]
+        id2excerpts = self.get_id2excerpts(excerpt_ids)
+
+        for article_id, excerpt_id in id2excerpts.items() : 
+            for e_id in excerpt_id : 
+                prompt = self.create_prompt(self.excerpts[e_id]['excerpt'])
+                label = self.create_prompt(self.excerpts[e_id]['type'])
+                yield (article_id, e_id , prompt, label)
+
+    def save_prediction(self, e_id, predicted_label, flush=False) : 
+        self.predicted_excerpts[e_id]['predicted_type'] = predicted_label
+
+            
+
+    def flush_predictions(self) : 
+        with open(self.path_to_save_predictions, 'wb') as f : 
+                pickle.dump(self.predicted_excerpts, f)
+
+
+        
+    def create_prompt(self, excerpt_to_predict) : 
+
+        example_texts = []
+
+        for excerpt_type in self.examples_for_each_label.keys() : 
+            example_text = ""
+            excerpt_example = random.sample(self.examples_for_each_label[excerpt_type] , 1)[0]
+
+            example_text += f"Newspaper Excerpt: {excerpt_example}\n"
+            example_text += f"Label: {excerpt_type}\n"
+
+            example_texts.append(example_text)
+
+        processed_prompt = self.prompt.replace("[DEFINITIONS]" , 
+                                               self.label_definitions)
+        
+        processed_prompt = processed_prompt.replace("[EXAMPLES]" , "\n".join(example_texts))
+
+        processed_prompt = processed_prompt.replace("[EXCERPT_TO_PREDICT]" , 
+                                                    f"Newspaper Excerpt: {excerpt_to_predict}\nLabel:")
+
+        return processed_prompt
+
+
+    def get_examples_for_each_label(self) : 
+
+        examples = {}
+        excerpt_split_for_examples = self.excerpts[self.train_split_to_use_for_prompt_example]["train"]
+
+        for excerpt_id, excerpt in excerpt_split_for_examples.items() : 
+
+            excerpt_label = excerpt["type"]
+            if excerpt_label not in examples : 
+
+                examples[excerpt_label] = [excerpt["excerpt"]]
+            else : 
+                examples[excerpt_label].append(excerpt["excerpt"])
+
+        return examples
+    
+    def get_id2excerpts(self, excerpt_ids) : 
+
+        id2excerpts = {}
+
+        for id in excerpt_ids : 
+            id = str(id)
+            for excerpt_sentence_id, excerpt_data in self.excerpts.items() : 
+                if id == excerpt_sentence_id.split('_')[0] : 
+                    if id not in id2excerpts : 
+                        id2excerpts[id] = [excerpt_sentence_id]
+                    else : 
+                        id2excerpts[id].append(excerpt_sentence_id)
+
+        return id2excerpts
+    
+
+
+            
+
+        
--- models/llama_chat/llama_chat_classifier.py
+++ models/llama_chat/llama_chat_classifier.py
@@ -1,16 +1,94 @@
-from transformers import LlamaTokenizer, LlamaForCausalLM
 
-PATH_TO_CONVERTED_WEIGHTS = ""
-PATH_TO_CONVERTED_TOKENIZER = ""
+'''
+1. Declare Variables
+'''
 
-model = LlamaForCausalLM.from_pretrained(PATH_TO_CONVERTED_WEIGHTS)
-tokenizer = LlamaTokenizer.from_pretrained(PATH_TO_CONVERTED_TOKENIZER)
+####~~~~ Llama Related Variables ~~~~~####
+LLAMA_MODULE_PATH = "/rc_scratch"
+LLAMA_CKPT_DIR = "/rc_scratch/"
+LLAMA_MODELS = ["/rc_scratch"]
 
-prompt = "Hey, are you conscious? Can you talk to me?"
-inputs = tokenizer(prompt, return_tensors="pt")
 
-# Generate
-generate_ids = model.generate(inputs.input_ids, max_length=30)
-tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+####~~~~ Data Related Variables ~~~~~####
+path_to_splits_dict = 'data/splits_dict'
+path_to_quant_dict = "data/quant_dict"
+path_to_save_predictions = "data/preds_dict"
+train_split_to_use_for_prompt_example = 0
+test_splits_to_predict = 0
 
+####~~~~ HyperParameters ~~~~~####
+max_seq_len = 1024
+max_gen_len = 16
+batch_size = 16
+temperature = 0.6 
+top_p = 0.9
+
+'''
+2. Module Imports
+'''
+####~~~~ Add Llama module to PATH for import ~~~~~####
+
+import sys 
+sys.path.insert(0, LLAMA_MODULE_PATH)
+
+
+####~~~~ Regular imports ~~~~~####
+
+import fire
+import os
+import random
+from itertools import product
+
+from models.llama_chat.data_handler import LlamaChatPromptGenerator
+
+from llama import Llama
+
+
+'''
+3. Setting up data
+'''
+
+prompts = LlamaChatPromptGenerator()
+
+
+'''
+4. Initializing Models
+'''
+
+LLAMA_TOKENIZER_PATH = os.path.join(LLAMA_MODULE_PATH , "tokenizer.model")
+
+generator = Llama.build(
+    ckpt_dir=LLAMA_CKPT_DIR,
+    tokenizer_path=LLAMA_TOKENIZER_PATH,
+    max_seq_len=max_seq_len,
+    max_batch_size=batch_size,
+)
+
+
+'''
+5. Running generator, saving data
+'''
+
+prompt_generator = prompts.generator(test_splits_to_predict)
+
+for article_id, excerpt_id, prompt, label in prompt_generator : 
+
+    print(f"ARTICLE ID : {article_id}")
+    print(f"EXCERPT ID : {excerpt_id}")
+    print(f"PROMPT : {prompt}")
+    print(f"LABEL : {label}")
+
+
+    prediction = generator.text_completion(
+            prompt,
+            max_gen_len=max_gen_len,
+            temperature=temperature,
+            top_p=top_p,
+        )
+
+    print(f"PREDICTION : {prediction}")
+
+    prompts.save_prediction(prediction)
+
+prompts.flush_predictions()
 
