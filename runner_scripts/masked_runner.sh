33f0c35df360d44bca08405f5383a6c3347d2969
aleto1999 <alexleto14@gmail.com>
2024-05-19 12:44:21 -0600

    updates to dapt

--- runner_scripts/masked_runner.sh
+++ runner_scripts/masked_runner.sh
@@ -27,4 +27,4 @@ export PYTHONPATH=/rc_scratch/alle5715/econ-indicators
 module load cuda
 module load cudnn
 
-python -m models.roberta_classifier.dapt
+python3 models/roberta_classifier/dapt.py --o models/roberta_classifier/tuned_models/roberta_base_dapt_512 --c roberta-base --s 512
