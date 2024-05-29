4f945ac7e714fc267d2e08eee912c9a60d83d5a7
aleto1999 <alexleto14@gmail.com>
2024-05-29 07:51:26 -0600

    adding collection1-1 reports, ready for 2-0

--- data_utils/visualization/generate_frame_dist_plot.py
+++ data_utils/visualization/generate_frame_dist_plot.py
@@ -6,6 +6,12 @@ import pandas as pd
 qual_dict = pickle.load(open('data/clean/qual_dict', 'rb'))
 quant_dict = pickle.load(open('data/clean/quant_dict', 'rb'))
 
+def sort_bars(labels, counts):
+    zipped = zip(labels, counts)
+    sorted_zipped = sorted(zipped, key=lambda x: x[1], reverse=True)
+    labels, counts = zip(*sorted_zipped)
+    return labels, counts
+
 def main():
 
         # QUAL ANNS
@@ -105,41 +111,62 @@ def main():
 
 
     fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex='col', constrained_layout=True)
-    fig.set_size_inches(5, 10)
+    fig.set_size_inches(10, 10)
     # plt.rc('font', size=20)
 
     # ax1.bar(frame_labels, frame_counts)
-    ax1.barh(frame_labels, frame_counts)
-    ax1.set_ylabel('Article Type')
-    ax1.get_yaxis().set_label_coords(-0.3, 0.5)
-    ax2.barh(econ_rate_labels, econ_rate_counts)
-    ax2.set_ylabel('Economic Conditions')
-    ax2.get_yaxis().set_label_coords(-0.3, 0.5)
-    ax3.barh(econ_change_labels, econ_change_counts)
-    ax3.set_ylabel('Economic Direction')
-    ax3.get_yaxis().set_label_coords(-0.3, 0.5)
+    frame_labels, frame_counts = sort_bars(frame_labels, frame_counts)
+    econ_rate_labels, econ_rate_counts = sort_bars(econ_rate_labels, econ_rate_counts)
+    econ_change_labels, econ_change_counts = sort_bars(econ_change_labels, econ_change_counts)
 
-    fig.supxlabel('Annotation Counts')
+    ax1.barh(frame_labels, frame_counts)
+    ax1.set_ylabel('Article Type', size=16)
+    # ax1.get_yaxis().set_label_coords(-0.3, 0.5)
+    ax2.barh(econ_rate_labels, econ_rate_counts)
+    ax2.set_ylabel('Economic Cond.', size=16)
+    # ax2.get_yaxis().set_label_coords(-0.3, 0.5)
+    ax3.barh(econ_change_labels, econ_change_counts)
+    ax3.set_ylabel('Economic Dir.', size=16)
+    # ax3.get_yaxis().set_label_coords(-0.3, 0.5)
+
+    fig.supxlabel('Annotation Counts', size=16)
+
+    tick_size = 14
+    ax1.tick_params(axis='both', which='major', labelsize=tick_size)
+    ax2.tick_params(axis='both', which='major', labelsize=tick_size)
+    ax3.tick_params(axis='both', which='major', labelsize=tick_size)
+
     fig.tight_layout()
+    fig.align_ylabels()
     plt.savefig('data_utils/visualization/results/frame_dist_qual.png', dpi=300)
 
     fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex='col', constrained_layout=True)
-    fig.set_size_inches(5, 10)
+    fig.set_size_inches(10, 10)
+
+    type_labels, type_counts = sort_bars(type_labels, type_counts)
+    spin_labels, spin_counts = sort_bars(spin_labels, spin_counts)
+    macro_type_labels, macro_type_counts = sort_bars(macro_type_labels, macro_type_counts)
 
     ax1.barh(type_labels, type_counts)
-    ax1.set_ylabel('Quantity Type')
-    ax1.get_yaxis().set_label_coords(-0.35, 0.5)
+    ax1.set_ylabel('Quantity Type', size=16)
+    # ax1.get_yaxis().set_label_coords(-0.35, 0.5)
     ax2.barh(spin_labels, spin_counts)
-    ax2.set_ylabel('Indicator Polarity')
-    ax2.get_yaxis().set_label_coords(-0.35, 0.5)
+    ax2.set_ylabel('Indicator Polarity', size=16)
+    # ax2.get_yaxis().set_label_coords(-0.35, 0.5)
     ax3.barh(macro_type_labels, macro_type_counts)
-    ax3.set_ylabel('Macro Indicator')
-    ax3.get_yaxis().set_label_coords(-0.35, 0.5)
+    ax3.set_ylabel('Macro Indicator', size=16)
+    # ax3.get_yaxis().set_label_coords(-0.35, 0.5)
     
 
     # Show graphic
-    fig.supxlabel('Annotation Counts')
+    tick_size = 14
+    ax1.tick_params(axis='both', which='major', labelsize=tick_size)
+    ax2.tick_params(axis='both', which='major', labelsize=tick_size)
+    ax3.tick_params(axis='both', which='major', labelsize=tick_size)
+
+    fig.supxlabel('Annotation Counts', size=16)
     fig.tight_layout()
+    fig.align_ylabels()
     # plt.show()
     plt.savefig('data_utils/visualization/results/frame_dist_quant.png', dpi=300)
 
