f782098ce5d73e5b1188fead7bcd9360d5f97216
aleto1999 <alexleto14@gmail.com>
2024-10-30 18:35:38 -0600

    final psl inference

---
+++ runner_scripts/psl_inference.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+#SBATCH --mem=64G
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --time=36:00:00
+#SBATCH --account=blanca-curc-gpu
+#SBATCH --qos=blanca-curc-gpu
+#SBATCH --partition=blanca-curc-gpu
+#SBATCH --output=logs/psl_final_inference-%j.out
+#SBATCH --mail-type="ALL"
+#SBATCH --mail-user="alle5715@colorado.edu"
+
+cd /scratch/alpine/alle5715/econ-indicators
+
+module load anaconda
+module load cuda/12.1.1
+
+mkdir -p logs
+nvidia-smi >> logs/nvidia-smi.out
+
+source /home/${USER}/.bashrc
+conda activate econ-indicators
+
+mkdir -p metadata
+mkdir -p outputs
+
+export TRANSFORMERS_CACHE=metadata/
+export PYTHONPATH=/scratch/alpine/alle5715/econ-indicators
+
+python3 -m models.psl.run_inference --s no_inter 
+python3 -m models.psl.eval.evaluate_inference --s no_inter
+python3 -m models.psl.eval.generate_setting_rule_table --s no_inter
