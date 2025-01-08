ce7c66dfec6caf5055d7c3f6e01debff2bda9606
aleto1999 <alexleto14@gmail.com>
2025-01-08 08:44:10 -0700

    added psl inference runners for faster inference

---
+++ runner_scripts/psl_inference3.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+#SBATCH --mem=32G
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --time=168:00:00
+#SBATCH --account=blanca-curc-gpu
+#SBATCH --qos=blanca-curc-gpu
+#SBATCH --partition=blanca-curc-gpu
+#SBATCH --output=logs/psl_final_inference-0-%j.out
+#SBATCH --mail-type="ALL"
+#SBATCH --mail-user="alle5715@colorado.edu"
+
+cd /scratch/alpine/alle5715/econ-indicators
+
+module load anaconda
+module load cuda/12.1.1
+
+mkdir -p logs
+nvidia-smi >> logs/nvidia-smi.out
+
+source /home/${USER}/.bashrc
+conda activate econ-indicators
+source env-psl/bin/activate
+
+mkdir -p metadata
+mkdir -p outputs
+
+export TRANSFORMERS_CACHE=metadata/
+export PYTHONPATH=/scratch/alpine/alle5715/econ-indicators
+
+for i in {15..19}; do
+    python3 -m models.psl.run_inference --s best_12-2024 --final_split $i
+done
+
