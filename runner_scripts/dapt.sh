8f3dc55d8e29ec0d0e2c795e71c974eb627c4f2f
aleto1999 <alexleto14@gmail.com>
2025-04-21 12:23:35 -0600

    renamed dapt runner script

--- runner_scripts/dapt.sh
+++ runner_scripts/dapt.sh
@@ -8,7 +8,7 @@
 #SBATCH --qos=blanca-curc-gpu
 #SBATCH --partition=blanca-curc-gpu
 #SBATCH --gres=gpu:1
-#SBATCH --output=logs/masked_tune-%j.out
+#SBATCH --output=logs/dapt-%j.out
 #SBATCH --mail-type="ALL"
 #SBATCH --mail-user="alle5715@colorado.edu"
 
