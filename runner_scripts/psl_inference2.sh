0a9c2af827554ad8dcfdb16c05a5814a3520f95e
aleto1999 <alexleto14@gmail.com>
2025-05-12 11:06:43 -0600

    final psl inference

--- runner_scripts/psl_inference2.sh
+++ runner_scripts/psl_inference2.sh
@@ -30,6 +30,6 @@ export TRANSFORMERS_CACHE=metadata/
 export PYTHONPATH=/scratch/alpine/alle5715/econ-indicators
 
 for i in {20..29}; do
-    python3 -m models.psl.run_inference --s best_12-2024 --final_split $i
+    python3 -m models.psl.run_inference --s best_2025-05 --final_split $i
 done
 
