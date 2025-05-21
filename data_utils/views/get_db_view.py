61318fab2705516dcb4145ce62d8abc6f86315ef
aleto1999 <alexleto14@gmail.com>
2025-05-21 09:50:17 -0600

    downloading new data ready

---
+++ data_utils/views/get_db_view.py
@@ -0,0 +1,73 @@
+import argparse
+import sqlite3
+import os
+
+
+def get_count(cursor, table_name, f):
+    """
+    Get the count of rows in a table.
+    """
+    cursor.execute(f"SELECT COUNT(*) FROM {table_name}")
+    count = cursor.fetchone()[0]
+    f.write(f"- row count: {count}\n")
+
+def get_publisher_article_count(cursor, f):
+    """
+    Get number of articles per publisher.
+    """
+    cursor.execute("SELECT source, COUNT(*) FROM article GROUP BY source")
+    rows = cursor.fetchall()
+    f.write("- per publisher:\n")
+    for row in rows:
+        f.write(f"  - {row[0]}: {row[1]}\n")
+
+def get_publisher_quantity_count(cursor, f):
+    """
+    Get number of quantities per publisher.
+    """
+    # join quantity with article to get the publisher
+    cursor.execute("""
+        SELECT a.source, COUNT(q.id) 
+        FROM quantity q
+        JOIN article a ON q.article_id = a.id
+        GROUP BY a.source
+    """)
+    rows = cursor.fetchall()
+    f.write("- per publisher:\n")
+    for row in rows:
+        f.write(f"  - {row[0]}: {row[1]}\n")
+
+def main(args):
+
+    # Connect to the SQLite database
+    conn = sqlite3.connect(args.db_name)
+    cursor = conn.cursor()
+
+    # create a file for the report
+    db_name = (args.db_name.split(".")[0]).split("/")[-1]
+    report_path = "data/reports"
+    os.makedirs(report_path, exist_ok=True)
+
+    report_file = os.path.join(report_path, f"{db_name}_report.txt")
+    with open(report_file, "w") as f:
+        f.write(f"Database Report: {db_name}\n")
+        f.write("================\n\n")
+
+        f.write("ARTICLE TABLE\n")
+        get_count(cursor, "article", f)
+        get_publisher_article_count(cursor, f)
+
+        f.write("\n\nQUANTITY TABLE\n")
+        get_count(cursor, "quantity", f)
+        get_publisher_quantity_count(cursor, f)
+
+
+    # Close the connection
+    conn.close()
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Database operations script')
+    parser.add_argument('--db', type=str, help='Path of the database')
+    args = parser.parse_args()
+    main(args)
---
+++ runner_scripts/download_data.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --mem=128G
+#SBATCH --time=48:00:00
+#SBATCH --account=blanca-curc-gpu
+#SBATCH --qos=blanca-curc-gpu
+#SBATCH --partition=blanca-curc-gpu
+#SBATCH --output=logs/download_new_data-%j.out
+#SBATCH --mail-type="ALL"
+#SBATCH --mail-user="alle5715@colorado.edu"
+
+cd /scratch/alpine/alle5715/econ-indicators
+
+module load anaconda
+module load cuda/12.1.1
+
+mkdir -p logs
+nvidia-smi >> logs/nvidia-smi.out
+
+source /home/${USER}/.bashrc
+conda activate econ-indicators
+
+mkdir -p metadata
+mkdir -p outputs
+
+export PYTHONPATH=/scratch/alpine/alle5715/econ-indicators
+
+python3 data_utils/collection/download/download_s3_data.py --start-date 2023-01-01 --end-date 2025-05-01 --output-dir data
