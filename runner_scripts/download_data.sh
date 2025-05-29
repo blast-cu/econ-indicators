289ca027200ce7c1a03b455eb0d3be495468991d
aleto1999 <alexleto14@gmail.com>
2025-05-29 08:47:07 -0600

    actually making changes this time

--- runner_scripts/download_data.sh
+++ runner_scripts/download_data.sh
@@ -27,4 +27,4 @@ mkdir -p outputs
 
 export PYTHONPATH=/scratch/alpine/alle5715/econ-indicators
 
-python3 data_utils/collection/download/download_s3_data.py --start-date 2023-01-01 --end-date 2023-12-31 --output-dir data
+python3 data_utils/collection/download/download_s3_data.py --start-date 2024-01-01 --end-date 2025-05-28 --output-dir data
