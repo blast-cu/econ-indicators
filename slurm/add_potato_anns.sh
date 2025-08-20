24e90fde33b5ccda7ca9b6723d319c7742fe71c7
aleto1999 <alexleto14@gmail.com>
2025-08-20 13:06:00 -0600

    added slurm script to add potato annotations to database

---
+++ slurm/add_potato_anns.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --time=1:00:00
+#SBATCH --account=blanca-curc-gpu
+#SBATCH --qos=blanca-curc-gpu
+#SBATCH --partition=blanca-curc-gpu
+#SBATCH --output=logs/add_potato_anns-%j.out
+#SBATCH --mail-type="ALL"
+#SBATCH --mail-user="alle5715@colorado.edu"
+
+cd /scratch/alpine/alle5715/econ-indicators
+
+module load anaconda
+module load cuda/12.1.1
+
+mkdir -p logs
+nvidia-smi >> logs/nvidia-smi.out
+
+source /home/${USER}/.bashrc
+conda activate econ-indicators
+
+mkdir -p metadata
+mkdir -p outputs
+
+export PYTHONPATH=/scratch/alpine/alle5715/econ-indicators
+
+python -m potato_annotation.add_anns
