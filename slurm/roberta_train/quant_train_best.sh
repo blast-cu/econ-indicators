6ec313e440cbf259de45dc123e68761a3397e333
aleto1999 <alexleto14@gmail.com>
2025-09-25 09:55:31 -0600

    added script to train best version of roberta

---
+++ slurm/roberta_train/quant_train_best.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=4
+#SBATCH --time=24:00:00
+#SBATCH --account=blanca-curc-gpu
+#SBATCH --qos=blanca-curc-gpu
+#SBATCH --partition=blanca-curc-gpu
+#SBATCH --gres=gpu:1
+#SBATCH --output=logs/quant_train_best.out
+#SBATCH --mail-type="ALL"
+#SBATCH --mail-user="alle5715@colorado.edu"
+
+cd /scratch/alpine/alle5715/econ-indicators
+
+module load anaconda
+module load cuda/12.1.1
+
+mkdir -p logs
+nvidia-smi >> logs/nvidia-smi.out
+
+source /home/${USER}/.bashrc
+conda activate econ-indicators
+
+mkdir -p metadata
+mkdir -p outputs
+
+export TRANSFORMERS_CACHE=metadata/
+export PYTHONPATH=/scratch/alpine/alle5715/econ-indicators
+
+python -m models.roberta_classifier.quant_train_best \
+    --type "quant_roberta_dapt_128" \
+    --macro_type "quant_roberta_dapt_128" \
+    --spin "quant_roberta_dapt_128"
