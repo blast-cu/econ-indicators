222474f35542f1a739417a4b753267c53437b080
aleto1999 <alexleto14@gmail.com>
2025-10-13 12:42:31 -0600

    increased time for preprocessing

--- slurm/psl_train/psl_rule_test.sh
+++ slurm/psl_train/psl_rule_test.sh
@@ -30,24 +30,24 @@ export TRANSFORMERS_CACHE=metadata/
 export PYTHONPATH=/scratch/alpine/alle5715/econ-indicators
 
 python3 models/psl/run_inference.py --s no_inter
-# python3 models/psl/eval/evaluate_inference.py --s no_inter
-# python3 models/psl/eval/generate_setting_rule_table.py --s no_inter
+python3 models/psl/eval/evaluate_inference.py --s no_inter
+python3 models/psl/eval/generate_setting_rule_table.py --s no_inter
 
 # python3 models/psl/run_inference.py --s no_inter --no_inter
 # python3 models/psl/eval/evaluate_inference.py --s no_inter
 # python3 models/psl/eval/generate_setting_rule_table.py --s no_inter
 
-# python3 models/psl/run_inference.py --s precedes
-# python3 models/psl/eval/evaluate_inference.py --s precedes
-# python3 models/psl/eval/generate_setting_rule_table.py --s precedes
+python3 models/psl/run_inference.py --s precedes
+python3 models/psl/eval/evaluate_inference.py --s precedes
+python3 models/psl/eval/generate_setting_rule_table.py --s precedes
 
-# python3 models/psl/run_inference.py --s excerpt_article
-# python3 models/psl/eval/evaluate_inference.py --s excerpt_article
-# python3 models/psl/eval/generate_setting_rule_table.py --s excerpt_article
+python3 models/psl/run_inference.py --s excerpt_article
+python3 models/psl/eval/evaluate_inference.py --s excerpt_article
+python3 models/psl/eval/generate_setting_rule_table.py --s excerpt_article
 
-# python3 models/psl/run_inference.py --s inter_article
-# python3 models/psl/eval/evaluate_inference.py --s inter_article
-# python3 models/psl/eval/generate_setting_rule_table.py --s inter_article
+python3 models/psl/run_inference.py --s inter_article
+python3 models/psl/eval/evaluate_inference.py --s inter_article
+python3 models/psl/eval/generate_setting_rule_table.py --s inter_article
 
 # python3 models/psl/run_inference.py --s combos
 # python3 models/psl/eval/evaluate_inference.py --s combos
